61b568f45927b12902cc05ba9a3896860615d7c6
Pt74bsx <pt74bsx@eduvaud.ch>
2026-09-04 11:19:26 +0200

    Drone mit en object dans une class

---
+++ personnel/DroneConsole/DroneConsole/Drone.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneConsole
+{
+    public class Drone
+    {
+        // Properties
+        private int _posX;
+        private int _posY;
+        private int _battery;
+
+        /// <summary>
+        /// Construction pérsonalisé du drone
+        /// </summary>
+        /// <param name="posX"></param>
+        /// <param name="posY"></param>
+        /// <param name="battery"></param>
+        public Drone(int posX, int posY, int battery)
+        {
+            this._posX = posX;
+            this._posY = posY;
+            this._battery = battery;
+        }
+
+        /// <summary>
+        /// Construction par défaut du drone 
+        /// </summary>
+        public Drone()
+        {
+            this._posX = 0;
+            this._posY = 10;
+            this._battery = 50;
+        }
+
+        /// <summary>
+        /// Vérifier l'état du drone 
+        /// </summary>
+        public bool oneIsAlive()
+        {
+            if (_battery == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Changer l'état du drone 
+        /// </summary>
+        public void changeState()
+        {
+            if (_battery > 0)
+            {
+                _posX = _posX + 1;
+                _battery = _battery - 2;
+            }
+        }
+
+        /// <summary>
+        /// Afficher le drone et sa batterie
+        /// </summary>
+        public void drawDrone()
+        {
+            Console.SetCursorPosition(_posX, _posY);
+            if (_battery == 0)
+            {
+                Console.Write("____");
+                Console.SetCursorPosition(_posX, _posY + 1);
+                Console.Write("Drone mort");
+            }
+            else
+            {
+                Console.Write("x-O-x");
+                Console.SetCursorPosition(_posX, _posY + 1);
+                Console.Write(" " + _battery + "%");
+            }   
+        }
+    }
+}
--- personnel/DroneConsole/DroneConsole/Program.cs
+++ personnel/DroneConsole/DroneConsole/Program.cs
@@ -24,90 +24,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DroneConsole
 {
     internal class Program
     {
-        // Déclarations des constantes
-        const int POS_Y = 20;           // Position Y du drone 
-
         static void Main(string[] args)
         {
-            // Déclarations des variables 
-            int pos_X = 0;              // Position X du drone 
-            int batterie = 50;          // Batterie du drone 
+            // Déclarations des objects 
+            Drone drone = new Drone(0, 10, 50);
 
-            Console.Clear();
-
+            // Paramètre 
             Console.CursorVisible = false;
-            Console.SetCursorPosition(pos_X, POS_Y);
-            Console.Write("x-O-x");
-            Console.SetCursorPosition(pos_X, POS_Y - 1);
-            Console.Write(batterie + "%");
 
-            System.Threading.Thread.Sleep(500);     // Pause de 0,5 sec
-
-            // Boucle qui se répéte tant que la battrie est plus grande que 0
-            while (batterie > 0)
+            // Boucle qui fait tourner les drones jusqu'à qui reste aucun drone 
+            while (drone.oneIsAlive())
             {
+                drone.changeState();
+                drone.drawDrone();
+                System.Threading.Thread.Sleep(500);             // Pause de 0,5 sec
+
                 Console.Clear();
-                Console.SetCursorPosition(pos_X, POS_Y);
-
-                // Affichage drone quand il a plus de batterie 
-                if (batterie == 2)
-                {
-                    Console.Write("____");
-                    Console.SetCursorPosition(pos_X, POS_Y - 1);
-                    Console.Write("Drone mort");
-                    System.Threading.Thread.Sleep(500);
-                    break;
-                }
-
-                // Drone fonctionne si la batterie est plus grande ou égal à 3
-                if (batterie >= 3)
-                {
-                    batterie = etatDrone(batterie);
-                    pos_X = DrawDrone(pos_X, POS_Y, batterie);
-                }
-
-                System.Threading.Thread.Sleep(500);  // Pause de 0,5 sec
             }
 
-            Console.ReadLine(); // Permet d'éviter la fermeture du programme (temporaire)
-        }
-
-        /// <summary>
-        /// Méthode qui permet de dessinner 
-        /// le drone et sa batterie
-        /// au bonne endroit 
-        /// </summary>
-        /// <param name="pos_X"></param>
-        /// <param name="POS_Y"></param>
-        /// <param name="batterie"></param>
-        /// <returns></returns>
-        static int DrawDrone(int pos_X, int POS_Y, int batterie)
-        {
-            pos_X = pos_X + 1;
-            Console.Write("x-O-x");
-            Console.SetCursorPosition(pos_X, POS_Y - 1);
-            Console.Write(batterie + "%");
-
-            return pos_X;
-        }
-
-        /// <summary>
-        /// Méthode qui permet de gérer 
-        /// l'état du drone comme la batterie
-        /// </summary>
-        /// <param name="batterie"></param>
-        /// <returns></returns>
-        static int etatDrone(int batterie)
-        {
-            batterie = batterie - 2;
-
-            return batterie;
+            Console.ReadKey();
         }
     }
 }
